74e3c4564f73af803c9cbaf9c8de767cd9d4d219
rkallepu <rashmika23@gmail.com>
2015-06-08 13:46:14 -0700

    Authentication Working

--- RideShare/modules/authentication/authentication-app.js
+++ RideShare/modules/authentication/authentication-app.js
@@ -1,49 +1,7 @@
-var mongoose = require('mongoose'),
-    passport = require('passport'),
+var passport = require('passport'),
     Account = require('./accounts'),
     express = require('express'),
     router = express.Router();
-
-router.get('/login', function(req, res) {
-    res.render('login', { user: req.user });
-});
-router.post('/login', passport.authenticate('local'), function(req, res) {
-    //console.log('hit request');
-    if(req.user){
-        //console.log('User Logged In');
-        res.status(200).json({user: {
-            name: req.user.username,
-            id: req.user._id
-        }});
-    }
-});
-router.get('/register', function(req, res) {
-    res.render('register', {});
-});
-
-router.post('/register', function(req, res, next) {
-    console.log('registering user');
-    console.log(req.body);
-    Account.register(new Account({ username: req.body.username }), req.body.password, function(err) {
-
-        if (err) { console.log('error while user register!', err); return next(err); }
-        else {
-            console.log('user registered!');
-            res.status(200).json({message: "User registered successfully"});
-        }
-    });
-});
-router.get('/logout', function(req, res) {
-    //req.session.destroy();
-    req.logout();
-    res.render('logout',{});
-    /*req.session.destroy(function (err) {
-     console.log(err);
-     res.redirect('/logout'); //Inside a callback… bulletproof!
-     });*/
-    //res.redirect('/logout');
-});
-module.exports = router;
 module.exports = function (app){
 
     app.use(passport.initialize());
@@ -60,4 +18,6 @@ module.exports = function (app){
     app.use('/register', require('./register'));
     app.use('/logout', require('./logout'));
     app.use('/info', require('./../homePage/info'));*/
+
+    app.use('/', require('./router'));
 };
